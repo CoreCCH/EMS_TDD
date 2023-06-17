f468f320ebb79bad566a485c2e240b010ad2982c
CoreCCH <j1618james.cm05g@nctu.edu.tw>
2023-06-17 16:19:51 +0800

    unittest

--- functional_tests.py
+++ functional_tests.py
@@ -1,6 +1,33 @@
 from selenium import webdriver
+import unittest
 
-browser = webdriver.Chrome()
-browser.get('http://localhost:8000')
+class NewVisitorTest(unittest.TestCase):
 
-assert 'The install worked successfully! Congratulations!' in browser.title
+    def setUp(self):
+        self.browser = webdriver.Chrome()
+        self.browser.implicitly_wait(3)
+
+    def tearDown(self):
+        self.browser.quit()
+
+    def test_can_show_weather(self):
+        # James聽到一個很酷的氣象查詢網頁
+        # 他要去看它的首頁
+        self.browser.get('http://localhost:8000')
+        
+        # 他發現網頁標題與標題顯示現在天氣
+        self.assertIn('Weather', self.browser.title)
+        self.fail('Finish the test!')
+
+        # 他受邀輸入一個城市地區
+        # (James住在新竹縣竹北市)
+
+        # 當他按下enter時，網頁會更新，現在網頁列出
+        # "現在天氣"
+
+        # 他很滿意的出門了
+
+
+if __name__ == '__main__':
+    unittest.main(warnings='ignore')
+
